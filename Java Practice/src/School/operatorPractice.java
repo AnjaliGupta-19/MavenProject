9605493dfa3ed71a444ac3e0e2e8579dfe26a4e9
Personal <Personal@DESKTOP-0D4P7SN.lan>
2024-10-15 22:01:15 +0530

    This is my first 3rd commit without any change

---
+++ Java Practice/src/School/operatorPractice.java
@@ -0,0 +1,29 @@
+package School;
+
+public class operatorPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("the sum is:"+60+40);
+		int x= 40;
+		int y = 30;
+//		System.err.println("the sum of"+x+"and"+y+"is: "+2);
+		//System.out.println("The value is:"+80-50);
+		
+		System.out.println("The vlaue of multiply is: "+20*4);
+		
+		if(true && false) {
+			System.out.println("Test");
+		}
+		
+		if(true || false) {
+			System.out.println("test2");
+		}
+		
+		if(	5<3 && 1>2 && 9<8 || 2>6 || 7>9) {
+			System.out.println("Test3");
+		}
+
+	}
+
+}
