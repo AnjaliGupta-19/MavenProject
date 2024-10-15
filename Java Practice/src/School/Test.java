a5342312ca32b43055aed01285c4f072a0ad1917
Personal <Personal@DESKTOP-0D4P7SN.lan>
2024-10-15 22:27:37 +0530

    Test.java commit

---
+++ Java Practice/src/School/Test.java
@@ -0,0 +1,17 @@
+package School;
+
+
+public class Test {
+	
+   static String name= "Anjali";
+  static  int id= 10;
+    
+
+	public static void main(String[] args) {
+		
+		Test t=new Test();
+		
+		System.out.println(t);
+	}
+
+}
